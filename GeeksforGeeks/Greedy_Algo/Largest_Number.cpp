fca5ee89bbc46939496f068ecba7e3fde00badd5
sweta_agarwal <sweta.agarwal96.sa@gmail.com>
2020-05-17 07:33:19 +0530

    Create Largest_Number.cpp

---
+++ GeeksforGeeks/Greedy_Algo/Largest_Number.cpp
@@ -0,0 +1,39 @@
+/*https://practice.geeksforgeeks.org/problems/largest-number-with-given-sum/0/
+    Largest Number with given Sum */
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int T,N,S;
+	cin>>T;
+	for(int i=1;i<=T;i++)
+	{
+	    cin>>N>>S;
+	    int remains,number[N]={0};
+	    remains=S;number;
+	    for(int j=0;j<N;j++)
+	    {
+	        if(remains>=9)
+	        {
+	            number[j]=9;
+	            remains=remains-9;
+	        }
+	        else
+	        {
+	            number[j]=remains;
+	            remains=0;
+	            break;
+	        }
+	    }
+	    if(remains!=0)
+	        cout<<"-1";
+	    else
+	    {
+    	    for(int j=0;j<N;j++)
+	            cout<<number[j];
+	    }
+	    cout<<endl;
+	}
+	return 0;
+}
