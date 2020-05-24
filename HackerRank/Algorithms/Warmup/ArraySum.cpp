c652c555352d66ee88ace7dbc04ebaa78f768dbf
sweta_agarwal <sweta.agarwal96.sa@gmail.com>
2020-05-24 20:01:02 +0530

    Delete ArraySum.cpp

--- HackerRank/Algorithms/Warmup/ArraySum.cpp
+++
@@ -1,9 +0,0 @@
-/* https://www.hackerrank.com/challenges/simple-array-sum/problem?h_r=next-challenge&h_v=zen
-      Simple Array Sum  */
-
-int simpleArraySum(vector<int> ar) {
-    int sum=0;
-    for(int a: ar)
-        sum+=a;
-    return sum;
-}
