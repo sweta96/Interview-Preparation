76076cd3d03218b998857470dae5bb753c32ec14
sweta_agarwal <sweta.agarwal96.sa@gmail.com>
2020-06-07 15:30:49 +0530

    Create TrappingRainWater.cpp

---
+++ LeetCode/Algorithms/TrappingRainWater.cpp
@@ -0,0 +1,28 @@
+/* https://leetcode.com/problems/trapping-rain-water/submissions/
+    Trapping Rain Water  */
+
+class Solution {
+public:
+    int max(int a,int b)
+    {
+        return (a>b)?a:b;
+    }
+    int trap(vector<int>& height) {
+        if(height.size()<=2)
+            return 0;
+        int result=0;
+        int l=0;
+        int r= height.size()-1;
+        int level=0,lower=0;
+        while(l<r)
+        {
+            if(height[l]<height[r])
+                lower=height[l++];
+            else
+                lower=height[r--];
+            level=max(level,lower);
+            result+=(level-lower);
+        }
+        return result;
+    }
+};
